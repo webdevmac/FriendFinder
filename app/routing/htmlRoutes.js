53fce498f5997d057469bdcf5bfa51c58422e460
Web-dev <webdevmac@users.noreply.github.com>
2019-02-06 19:58:35 -0500

    update

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,6 +1,7 @@
 var path = require("path");
 
- 
+var friends = require("../data/friends.js");
+
 
 module.exports = function(app) {
  
@@ -9,10 +10,15 @@ app.get('/survey', function(req, res){
     res.sendFile(path.join(__dirname,"../public/survey.html"));
     
 });
+app.get("/api/friends", function (req, res) {
+    // console.log("dm1");
+    res.json(friends);
+  });
 //home route
 app.get('*', function(req, res){
     res.sendFile(path.join(__dirname,"../public/home.html"));
 });
+
 
 }
   
